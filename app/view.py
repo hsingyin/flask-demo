45b5b6c71bfb655ce1ae213a6621cf3288db3e29
HsingYin <582136509@qq.com>
2018-07-13 11:12:03 +0800

    修改了视图函数，删除无用文件

--- app/view.py
+++ app/view.py
@@ -62,9 +62,6 @@ def about():
 def contact():
     return render_template('contact.html')
 
-# # 404
-# @app.errorhandler(404)
-# def page_not_found(error):
-#     return render_template('404.html')
-# if __name__ == '__main__':
-#     app.run(debug = True)
+@app.errorhandler(404) 
+def page_not_found(e):     
+    return render_template('404.html'), 404 

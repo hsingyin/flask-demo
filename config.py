46d743645be467c5dfa62fcbe3be368ea98ac42b
hsingyin <582136509@qq.com>
2018-07-13 09:07:20 +0800

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-import hashlib
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-CSRF_ENABLED = True
-SECRET_KEY = 'hsingyin'
-# 定义上传文件的路径和允许上传的文件格式
-ALLOWED_EXTENSIONS = set(['txt','png','jpg','xls','JPG','PNG','xlsx','gif','GIF','mp3'])
-UPLOAD_FOLDER='../uploads/files'
